e088d31fc92ffbd5f2fdf10e094ddb176fa2a790
Uriel Quiroz <uriel.quiroz187@gmail.com>
2021-08-13 12:25:22 -0500

    Validando el formulario
    Validando que los campos no estén vacíos

--- js/app.js
+++ js/app.js
@@ -8,5 +8,40 @@ window.addEventListener('load', () => {
 
 function buscarClima(e) {
     e.preventDefault();
-    console.log('buscando clima...')
+    
+    //Validando
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    if(ciudad === '' || pais === '') {
+        //Hubo un erro
+        mostrarErro('Ambos campos son obligatorios');
+        return;
+    }
+
+}
+
+
+function mostrarErro(msj) {
+    const alertaMsj = document.querySelector('.msjAlerta');
+
+    if(!alertaMsj) {
+        //Crea una alerta
+        const divAlert = document.createElement('div');
+
+        divAlert.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md',
+                                    'mx-auto', 'mt-6', 'text-center', 'msjAlerta');
+        
+        divAlert.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block">${msj}</span>
+        `;
+
+        container.appendChild(divAlert);
+
+        //Se elimine la alerta despues de 3 segundo
+        setTimeout(() => {
+            divAlert.remove();
+        }, 3000);
+    }
 }
